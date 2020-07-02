4d5d7a676cdbf870bb9a91e030a9372c55bada0f
VitorAndreo <vitorsaraivaandreo@gmail.com>
2020-07-02 17:08:34 -0300

    Prototipos: H, F, H2O, O2

---
+++ Assets/RodarRedor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RodarRedor : MonoBehaviour
+{
+    public Transform tagert;
+    public int velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (tagert == null)
+        {
+            tagert = this.gameObject.transform;
+            Debug.Log("Destino RodarRedor não especificado. Padronize o objeto pai");
+
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(tagert.transform.position, tagert.transform.up, velocidade * Time.deltaTime);
+
+    }
+}
